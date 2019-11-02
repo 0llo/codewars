858ed2006228f53c46ebed0042ded43ac99b8146
conan <conan@ecpmail.com>
2019-11-01 17:30:54 -0700

    submit first final

--- test/index.js
+++ test/index.js
@@ -1,10 +1,30 @@
-function test(input) {
-  const arr = input.split(" ");
-  const arrNum = arr.map(word => {
-    return word.length;
-  });
-
-  return Math.min.apply(null, arrNum);
+function test(dna) {
+  const arr = dna.split("");
+  const result = arr
+    .map(item => {
+      switch (item) {
+        case "A":
+          return "T";
+          break;
+        case "T":
+          return "A";
+          break;
+        case "C":
+          return "G";
+          break;
+        case "G":
+          return "C";
+          break;
+        default:
+          return "error";
+          break;
+      }
+    })
+    .join("");
+  return result;
 }
-const input = "bitcoin take over the world maybe who knows perhaps";
-console.log(test(input));
+const pairs = { A: "T", T: "A", C: "G", G: "C" };
+
+const dna = "TAACG";
+const DNAStrand = dna => dna.replace(/./g, a => pairs[a]);
+console.log(DNAStrand(dna));
