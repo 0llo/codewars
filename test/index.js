5c9069c2198dbce695b6f24f0506289f0de7c93a
0llo <conan@ecpmail.com>
2020-01-17 17:51:34 -0800

    test

--- test/index.js
+++ test/index.js
@@ -1,58 +1,15 @@
-// const permutation = arr =>
-//   arr.length > 1
-//     ? arr.reduce(
-//         (acc, val, idx) => [
-//           ...acc,
-//           ...permutation(arr.filter((_, i) => i !== idx)).map(v => [val, ...v])
-//         ],
-//         []
-//       )
-//     : [arr];
-
-// const innerP = pres => xs => k =>
-//   k === 0
-//     ? [pres]
-//     : xs.reduce(
-//         (acc, e, i) => [
-//           ...acc,
-//           ...innerP([...pres, e])(xs.filter((_, j) => j !== i))(k - i)
-//         ],
-//         []
-//       );
-
-// const innerA = pres => xs => k =>
-//   k === 0
-//     ? [pres]
-//     : xs.reduce(
-//         (acc, e, i) => [
-//           ...acc,
-//           ...innerA([...pres, e])(xs.filter((_, j) => j !== i))(k - 1)
-//         ],
-//         []
-//       );
-
-// const perm = innerP([]);
-// const permA = innerA([]);
-
-// console.log(perm([0, 1, 2, 3])(4));
-// console.log(permA([0, 1, 2, 3])(4));
-// console.log(permutation([0, 1, 2, 3]));
-
-let arrs = [];
-const test = (arr, pre = []) => {
-  if (arr.length === 0) {
-    console.log(`now arr.length===0`);
-    arrs.push([pre]);
-    console.log(`arrs : ${arrs}`);
-    return;
+const generate = numRows => {
+  const result = [];
+  const numRowsH = Math.ceil(numRows / 2);
+  for (let i = 1; i <= numRows; i++) {
+    let arr = [];
+    for (let j = 1; j <= i; j++) arr.push(j);
+    result.push(arr);
   }
-  for (let i = 0; i < arr.length; i++) {
-    const copyArr = arr.slice();
-    console.log(`i: ${i}, arr: ${arr}, pre: ${pre}, arrs: ${arrs}`);
-    pre.push(copyArr[i]);
-    test([...arr.slice(0, i), ...arr.slice(i + 1)], pre);
-    pre.pop();
-  }
+
+  for (let i = 1; i <= numRowsH; i++) {}
+
+  return result;
 };
 
-console.log(test([2, 4, 5]));
+console.log(generate(5));
